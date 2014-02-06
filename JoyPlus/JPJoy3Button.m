d9b993722eef52be0d58f4eda7fa63fb62b3220c
Ciel <imwithye@gmail.com>
2014-02-06 12:46:53 +0800

    JPJoy3Button buttons link action.

--- JoyPlus/JPJoy3Button.m
+++ JoyPlus/JPJoy3Button.m
@@ -21,16 +21,19 @@
         
         self.buttonOne = [[SKButton alloc] initWithImageNamedNormal:@"Button1.png" selected:@"Button1Sel.png"];
         [self.buttonOne setPosition:CGPointMake(size.width - 40, 150)];
+        [self.buttonOne link];
         [self.controllers addObject:self.buttonOne];
         [self addChild:self.buttonOne];
         
         self.buttonTwo = [[SKButton alloc] initWithImageNamedNormal:@"Button2.png" selected:@"Button2Sel.png"];
         [self.buttonTwo setPosition:CGPointMake(size.width - 90, 120)];
+        [self.buttonTwo link];
         [self.controllers addObject:self.buttonTwo];
         [self addChild:self.buttonTwo];
         
         self.buttonThree = [[SKButton alloc] initWithImageNamedNormal:@"Button3.png" selected:@"Button3Sel.png"];
         [self.buttonThree setPosition:CGPointMake(size.width - 140, 90)];
+        [self.buttonThree link];
         [self.controllers addObject:self.buttonThree];
         [self addChild:self.buttonThree];
     }
