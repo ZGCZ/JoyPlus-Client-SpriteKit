59192b61f98530250571ea5082dfa74ea254ccac
Ciel <imwithye@gmail.com>
2014-02-10 11:23:40 +0800

    add updateTouchPoint method into JPDrag class.

--- JoyPlus/JPDrag.m
+++ JoyPlus/JPDrag.m
@@ -13,6 +13,8 @@
     SKShapeNode *line;
 }
 
+- (void)updateTouchPoint:(UITouch*)touches;
+
 @end
 
 @implementation JPDrag
@@ -37,6 +39,7 @@
     [super touchesMoved:touches withEvent:event];
     UITouch* touch = [touches anyObject];
     CGPoint positionInScene = [touch locationInNode:self];
+    [self updateTouchPoint:touch];
     CGPathAddLineToPoint(pathToDraw, NULL, positionInScene.x, positionInScene.y);
     line.path = pathToDraw;
 }
@@ -46,6 +49,12 @@
     [super touchesEnded:touches withEvent:event];
     [line removeFromParent];
     CGPathRelease(pathToDraw);
+}
+
+- (void)updateTouchPoint:(UITouch*)touch
+{
+    CGPoint positionInScene = [touch locationInNode:self];
+    NSLog(@"Position changed: (%f, %f)", positionInScene.x, positionInScene.y);
 }
 
 @end
