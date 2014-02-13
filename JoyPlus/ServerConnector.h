483e5f30233c8a01417572adabe7bbcba10310ac
Zhou Xinzi <imdreamrunner@gmail.com>
2014-02-13 21:36:22 +0800

    import lib

---
+++ JoyPlus/ServerConnector.h
@@ -0,0 +1,13 @@
+//
+//  ServerConnector.h
+//  JoyPlus
+//
+//  Created by Zhou Xinzi on 13/2/14.
+//  Copyright (c) 2014 Ciel Breiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ServerConnector : NSObject
+
+@end
---
+++ JoyPlus/ServerConnector.m
@@ -0,0 +1,14 @@
+//
+//  ServerConnector.m
+//  JoyPlus
+//
+//  Created by Zhou Xinzi on 13/2/14.
+//  Copyright (c) 2014 Ciel Breiz. All rights reserved.
+//
+
+#import "ServerConnector.h"
+#import "SRWebSocket.h"
+
+@implementation ServerConnector
+
+@end
