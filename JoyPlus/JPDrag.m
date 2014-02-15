db44772a426571333094dd823fe7ecfb41f6373e
Ciel <imwithye@gmail.com>
2014-02-16 01:55:53 +0800

    New JPDrag

--- JoyPlus/JPDrag.m
+++ JoyPlus/JPDrag.m
@@ -7,10 +7,12 @@
 //
 
 #import "JPDrag.h"
+#import "JPServerConnector.h"
 
 @interface JPDrag (){
     CGMutablePathRef pathToDraw;
     SKShapeNode *line;
+    double originX, originY;
 }
 
 - (void)updateTouchPoint:(UITouch*)touches;
@@ -24,6 +26,8 @@
     [super touchesBegan:touches withEvent:event];
     UITouch* touch = [touches anyObject];
     CGPoint positionInScene = [touch locationInNode:self];
+    originX = positionInScene.x;
+    originY = positionInScene.y;
     
     pathToDraw = CGPathCreateMutable();
     CGPathMoveToPoint(pathToDraw, NULL, positionInScene.x, positionInScene.y);
@@ -54,7 +58,11 @@
 - (void)updateTouchPoint:(UITouch*)touch
 {
     CGPoint positionInScene = [touch locationInNode:self];
-    NSLog(@"Position changed: (%f, %f)", positionInScene.x, positionInScene.y);
+    NSLog(@"Position changed: (%f, %f)", positionInScene.x - originX, positionInScene.y - originY );
+    JPServerConnector *jps = [JPServerConnector instance];
+    [jps send: [NSString stringWithFormat:@"{\"event\":\"drag\",\"x\":%f,\"y\":%f}",
+                positionInScene.x - originX,
+                positionInScene.y - originY]];
 }
 
 @end
