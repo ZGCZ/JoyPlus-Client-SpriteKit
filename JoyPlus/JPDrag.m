7d6c5e50b38e34d469f1aa96269838e4455637bd
Ciel <imwithye@gmail.com>
2014-02-16 02:51:19 +0800

    Finally JPDrag added

--- JoyPlus/JPDrag.m
+++ JoyPlus/JPDrag.m
@@ -29,6 +29,11 @@
     originX = positionInScene.x;
     originY = positionInScene.y;
     
+    JPServerConnector *jps = [JPServerConnector instance];
+    [jps send: [NSString stringWithFormat:@"{\"event\":\"dragStart\",\"x\":%f,\"y\":%f}",
+                .0,
+                .0]];
+    
     pathToDraw = CGPathCreateMutable();
     CGPathMoveToPoint(pathToDraw, NULL, positionInScene.x, positionInScene.y);
     
@@ -51,6 +56,12 @@
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [super touchesEnded:touches withEvent:event];
+    UITouch* touch = [touches anyObject];
+    CGPoint positionInScene = [touch locationInNode:self];
+    JPServerConnector *jps = [JPServerConnector instance];
+    [jps send: [NSString stringWithFormat:@"{\"event\":\"dragEnd\",\"x\":%f,\"y\":%f}",
+                positionInScene.x - originX,
+                -(positionInScene.y - originY)]];
     [line removeFromParent];
     CGPathRelease(pathToDraw);
 }
@@ -58,11 +69,11 @@
 - (void)updateTouchPoint:(UITouch*)touch
 {
     CGPoint positionInScene = [touch locationInNode:self];
-    NSLog(@"Position changed: (%f, %f)", positionInScene.x - originX, positionInScene.y - originY );
+    NSLog(@"Position changed: (%f, %f)", positionInScene.x - originX, -(positionInScene.y - originY));
     JPServerConnector *jps = [JPServerConnector instance];
-    [jps send: [NSString stringWithFormat:@"{\"event\":\"drag\",\"x\":%f,\"y\":%f}",
+    [jps send: [NSString stringWithFormat:@"{\"event\":\"dragMove\",\"x\":%f,\"y\":%f}",
                 positionInScene.x - originX,
-                positionInScene.y - originY]];
+                -(positionInScene.y - originY)]];
 }
 
 @end
