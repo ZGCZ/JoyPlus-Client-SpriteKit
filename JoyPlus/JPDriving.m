b0f1ea65315658464599487a681a884226ed3d94
Zhou Xinzi <imdreamrunner@gmail.com>
2014-02-15 21:28:17 +0800

    Add new JPDriving

---
+++ JoyPlus/JPDriving.m
@@ -0,0 +1,13 @@
+//
+//  JPDriving.m
+//  JoyPlus
+//
+//  Created by Zhou Xinzi on 15/2/14.
+//  Copyright (c) 2014 Ciel Breiz. All rights reserved.
+//
+
+#import "JPDriving.h"
+
+@implementation JPDriving
+
+@end
